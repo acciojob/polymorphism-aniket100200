7ceb4993efae64e7d6b1a67edc88d73694204fa6
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-07-12 09:30:39 +0530

    All changes Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class Main {
+import com.sun.nio.sctp.SctpSocketOption;
 
+public class Main
+{
+    static class Product{
+        public Product(){}
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+        public  int product(int x,int y,int z){
+            return x*y*z;
+        }
+    }
+
+    public static void main(String[]args)
+    {
+        Product p=new Product();
+       System.out.println( p.product(10,20));
+    }
 }
